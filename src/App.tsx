e37deca3dad6b6292d2b3c727c02b2e537294b80
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-10-13 13:36:46 +0000

    setup project

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { FC, ReactElement } from "react";
 
 interface AppProps {}
 
-const App: FC<AppProps> = ({}): ReactElement => {
+const App: FC<AppProps> = (): ReactElement => {
   return <div></div>;
 };
 
